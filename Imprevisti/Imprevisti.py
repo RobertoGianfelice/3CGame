290f13ff5a8e3b0277cff9c17a86b7605bc3aad1
Roberto Gianfelice <rob.gianfelice@gmail.com>
2018-04-07 10:39:49 +0200

    Update Imprevisti.py
    Supporto classe scritte

--- Imprevisti/Imprevisti.py
+++ Imprevisti/Imprevisti.py
@@ -1,74 +1,60 @@
 
+from Scritte import Scritte
+
 class ImprevistiClass(object):
     
+    def __init__(self, speed, sizeImpr):
     
-    
-    def __init__(self, speed):
-        
         global imprevistiArray
         global imprevistiValoreDictionary
-        global imprevistoSize
-        global canvaHeight
-        global canvaWidth
-        
-        canvaWidth = width
-        canvaHeight = height
-        
         
         self.speed = speed
         self.punteggio = 0
-        self.isItUsed = False 
+        self.sizeImpr = sizeImpr
         
-        imprevistoSize = 75
         imprevistiArray = ['Banana-20.png','Razzo-100.png','BombaGameOver.png','Bonus100.png','Bonus50.png','Banana-60.png']
         imprevistiValoreDictionary = {'Banana-20.png': -20,'Razzo-100.png': -100,'BombaGameOver.png' : 0,'Bonus100.png' : 100,'Bonus50.png' : 50,'Banana-60.png' : -60}
         
-        self.x = canvaWidth-imprevistoSize + random(500)
-        self.y = random(canvaHeight - imprevistoSize) 
+        self.x = width-self.sizeImpr + random(500)
+        self.y = random(height - self.sizeImpr) 
         
         self.createNewImprevisto()
         
     def moveImprevisto(self):
-        global imprevistoSize 
-        global canvaHeight
-        global canvaWidth
-        
         self.x -= self.speed 
         
-        if self.x < -imprevistoSize:
+        if self.x < -self.sizeImpr:
             self.createNewImprevisto()
-            if self.speed < 7:
-                self.speed += 0.50
-        
-        image(imprevisto, self.x, self.y, imprevistoSize, imprevistoSize)
-            
-        textSize(15)
-        
-        if self.punteggio < 0:
-            fill(255, 7,7)
-        elif self.punteggio > 0:
-            fill(135, 255, 84)
-        else:
-            self.punteggio = '' 
+            if self.speed < 5:
+                self.speed += 0.15
+    
+        colore = ''
 
-        text(self.punteggio, self.x + imprevistoSize, self.y + imprevistoSize)
-        
-        
+        if self.punteggio < 0:
+            colore = 'red'
+        elif self.punteggio > 0:
+            colore = 'green'
+        else:
+            self.punteggio = ''
+
+        punteggioScritta = Scritte(self.punteggio, self.x + self.sizeImpr, self.y + self.sizeImpr, colore, 15)
+        image(imprevisto, self.x, self.y, self.sizeImpr, self.sizeImpr) 
+
+    
     def createNewImprevisto(self):
-        global imprevisto 
-        global imprevistiValoreDictionary
+        global imprevisto
         
-        self.x = canvaWidth-imprevistoSize + random(500)
-        self.y = random(canvaHeight - imprevistoSize)
+        self.x = width-self.sizeImpr + random(500)
+        self.y = random(height - self.sizeImpr)
         
         imprevistoType = imprevistiArray[int(random(len(imprevistiArray)))]
         imprevisto = loadImage(imprevistoType)
         self.punteggio = imprevistiValoreDictionary[imprevistoType]
-        self.isItUsed = False
+        
         
     def collisione(self):
-        self.isItUsed = True
         self.createNewImprevisto()
+            
             
         
     
