c2d8a21fea8025671536cf9f6084ced3f58ac154
Roberto Gianfelice <rob.gianfelice@gmail.com>
2018-04-09 10:36:08 +0200

    Update Mezz.py
    Aggiunta scritta nella mezzeria con movimento. Chiedere al team Scritte di poter settare la posizione della scritta

--- Mezzeria/Mezz.py
+++ Mezzeria/Mezz.py
@@ -1,11 +1,14 @@
 class mezz(object):
     coloreStriscia=color(128,128,128)
-    def __init__(self,x,step,speed,larg,altez):
+    def __init__(self,x,step,speed,larg,altez,testo=''):
         self.x = x
         self.step = step
         self.speed = speed
         self.larg = larg
         self.altezza= altez
+        self.testo=testo
+        self.scritta=Scritte(self.testo,self.x,250,0,18)       
+        
         
     def move(self):
         self.x = self.x - self.step*self.speed
@@ -15,9 +18,12 @@ class mezz(object):
                     
     def display(self):
         for i in  range(width/10):
-            fill(mezz.coloreStriscia)
-            
+            fill(mezz.coloreStriscia)            
             rect(self.x + i*120,height/2,self.larg,self.altezza)
+            #self.scritta.SetPos(self.x + i*120+10,height/2+13)
+            self.scritta.x=self.x + i*120+17
+            self.scritta.y=height/2+17
+            self.scritta.display()
             
         
         
