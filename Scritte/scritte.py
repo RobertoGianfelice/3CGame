4e41cb9f1c3650c36ca9d0784d7e7c04da642885
Roberto Gianfelice <rob.gianfelice@gmail.com>
2018-04-14 10:19:59 +0200

    Update scritte.py
    Aggiunta metodo SetPos e Colore

--- Scritte/scritte.py
+++ Scritte/scritte.py
@@ -1,24 +1,25 @@
-class Scritte(object):
+ class Scritte(object):
     
-    def __init__(self,testo,x,y, colore, fontSize):
+    def __init__(self,testo, colore, fontSize):   # quando inizializziamo una classe dobbiamo dire 'cosa volgiamo scrivere' il colore che gli vogliamo dare con 'red black or green' e la grandezza del testo
         self.testo=testo
-        self.x = x
-        self.y=y
         self.colore = colore 
         self.fontSize = fontSize
 
     def display(self):
         textSize(self.fontSize)
-        fill(self.colore)
-        
         
         if self.colore=='green':
             fill(135,255,84)
         elif self.colore== 'red':
             fill(255,7,7)
-        else:
+        elif self.colore== 'black':
             fill(0)
         text(self.testo,self.x,self.y)
     
     def updateTesto(self,testo):
         self.testo=testo
+    
+    def setPos(self,x,y):  # decidiamo dove posizionare la scritta 
+        self.x=x
+        self.y=y
+        self.display()
