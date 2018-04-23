e42ecd8342e879f984bc3851ede670c2aac52aa1
Roberto Gianfelice <rob.gianfelice@gmail.com>
2018-04-23 10:26:40 +0200

    Update Mezz.py

--- Mezzeria/Mezz.py
+++ Mezzeria/Mezz.py
@@ -1,31 +1,34 @@
+from Scritte import Scritte
+
 class mezz(object):
     coloreStriscia=color(128,128,128)
-    def __init__(self,x,step,speed,larg,altez,testo=''):
+    def __init__(self,x,step,speed,larg,altez,testo="3C"):
         self.x = x
         self.step = step
         self.speed = speed
         self.larg = larg
         self.altezza= altez
-        self.testo=testo
-        self.scritta=Scritte(self.testo,self.x,250,0,18)       
+        #self.colore=colore
+        #self.fontSize=fontSize
         
+        self.scritta=Scritte(testo,"red",self.altezza-1)
+                            
         
     def move(self):
         self.x = self.x - self.step*self.speed
         
-        if self.x < -120                                                                                                                                            :
+        if self.x < -120:
             self.x = 0
                     
     def display(self):
-        for i in  range(width/10):
-            fill(mezz.coloreStriscia)            
-            rect(self.x + i*120,height/2,self.larg,self.altezza)
-            #self.scritta.SetPos(self.x + i*120+10,height/2+13)
-            self.scritta.x=self.x + i*120+17
-            self.scritta.y=height/2+17
-            self.scritta.display()
-            
         
+        
+        for i in  range(width/10):
+            fill(mezz.coloreStriscia)
+            rect(self.x + i*120,height/2,self.larg,self.altezza)
+      
+            self.scritta.setPos(self.x + i*120 + 3,height/2+self.altezza-2)
+            self.scritta.display()
         
         
    
