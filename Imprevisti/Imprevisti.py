1e1b7e21114dea7d17d1cd7dd6d0d9329db8dc4a
Roberto Gianfelice <rob.gianfelice@gmail.com>
2018-04-23 09:49:53 +0200

    Update Imprevisti.py
    Migliorata gestione scritte
    Gestione colore

--- Imprevisti/Imprevisti.py
+++ Imprevisti/Imprevisti.py
@@ -2,31 +2,45 @@
 from Scritte import Scritte
 
 class Imprevisti(object):
+    
     def __init__(self, speed, sizeImpr):
       
         self.speed = speed
         self.punteggio = 0
         self.sizeImpr = sizeImpr
         self.imprevistiArray = ['-20.png','-100.png','0.png','100.png','50.png','-60.png']
-    
+        
+        self.punteggioScritta = Scritte(self.punteggio,'', 15)
         
     
         
     def moveImprevisto(self):
         
         self.x -= self.speed 
-        
         if self.x < -self.sizeImpr:
             self.createNewImprevisto()
             if self.speed < 5:
                 self.speed += 0.15
                 
         image(imprevisto, self.x, self.y, self.sizeImpr, self.sizeImpr)
-        #punteggioScritta = Scritte(self.punteggio, self.x + self.sizeImpr, self.y + (self.sizeImpr + 15)/2, self.coloreScritta(), 15)
+        
+        colore = color(255)
+        
+        if int(self.punteggio) > 0:
+           colore = color(135,255,84)
+        elif int(self.punteggio) < 0:
+           colore = color(255, 7,7)
+        else:
+            colore = color(255)
+            
+        self.punteggioScritta.colore = colore
+        self.punteggioScritta.updateTesto(self.punteggio)
+        self.punteggioScritta.setPos(self.x + self.sizeImpr, self.y + (self.sizeImpr + 15)/2)   
+        
+        
         
     def createNewImprevisto(self):
         global imprevisto
-        global punteggioScritta
         
         self.x = width-self.sizeImpr + random(500)
         self.y = random(height - self.sizeImpr) 
@@ -34,18 +48,6 @@ class Imprevisti(object):
         numeroImprevisto = int(random(len(self.imprevistiArray))) # Posizione dell'imprevisto all'interno di imprevistiArray
         imprevisto = loadImage(self.imprevistiArray[numeroImprevisto]) #carica l'immagine dell'imprevisto
         self.punteggio = self.imprevistiArray[numeroImprevisto].replace('.png', '') #siccome il  nome di ogni immagina rappresenta il punteggio dell'imprevisto, prendo il nome dell'imprevisto e rimuovo .png
-        punteggioScritta = Scritte(self.punteggio, self.x + self.sizeImpr, self.y + (self.sizeImpr + 15)/2, self.coloreScritta(), 15)
-        
-        
-        
-        
-    def coloreScritta(self):  #sceglie il colore della scritta in base al punteggio, se e' negativo il colore e' rosso altrimenti il punteggio e' verde
-        if self.punteggio < 0:
-            return 'red'
-        elif self.punteggio > 0:
-            return 'green'
-        else:
-            return 'transparente'    
         
         
     def collisione(self):
