8281c35b19cb8bcc090077dbd28a1141ac509fde
Roberto Gianfelice <rob.gianfelice@gmail.com>
2018-04-24 11:33:19 +0200

    Update 3CGame_Main.py
    Allineamento alle nuove versione delle classi e aggiunta vettore imprevisti

--- 3CGame/3CGame_Main.py
+++ 3CGame/3CGame_Main.py
@@ -1,18 +1,26 @@
 from Mezzeria import mezz
-from Imprevisti import ImprevistiClass
+from Imprevisti import Imprevisti
 from Scritte import Scritte
 from Macchina import car
 
 larghezzaSchermo=800
 altezzaSchermo=500
-mezzeria=mezz(0,1,1,60,20,"3C")
-imprevisto=ImprevistiClass(1)
-punteggio=Scritte(0,larghezzaSchermo/2,15,0,18)
-macchina=car(10,10,1,2)
+speed=4
+dimImprevisto=larghezzaSchermo/20
+mezzeria=mezz(0,1,speed,larghezzaSchermo*9.0/100,altezzaSchermo*4.0/100)
+ListaImpr=[]
+imprevisto=Imprevisti(speed,dimImprevisto)
+ListaImpr.append(imprevisto)
+punteggio=Scritte('0','green',18)
+
+macchina=car(10,10,speed*2,1)
 
 def setup():
   size(larghezzaSchermo,altezzaSchermo)
-  imprevisto.inizializza(1)
+  for impr in ListaImpr:
+      impr.createNewImprevisto()
+  punteggio.setPos(larghezzaSchermo/2,15)
+  frameRate(30)
 
 def draw():
   background(255)
@@ -21,9 +29,20 @@ def draw():
   mezzeria.move()
   mezzeria.display()
 
-  imprevisto.moveImprevisto()  #Sposta e visualizza
+  for impr in ListaImpr:
+      impr.moveImprevisto()
+
   macchina.display()
   
   if (keyPressed):
       macchina.comandi(key)
+
+  for impr in ListaImpr:
+    if (macchina.collisione(impr)):
+      punteggio.updateTesto(str(int(punteggio.testo)+impr.getValue()))
+      impr.collisione()
+      
+  
+  
+  
   
