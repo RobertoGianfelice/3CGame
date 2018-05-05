43510b9f32a3902135936baf045c6381ab825411
Roberto Gianfelice <rob.gianfelice@gmail.com>
2018-05-05 10:35:16 +0200

    Update Mezz.py
    aggiunto il metodo per modificare la velocità dall'esterno

--- Mezzeria/Mezz.py
+++ Mezzeria/Mezz.py
@@ -1,19 +1,16 @@
 from Scritte import Scritte
 
 class mezz(object):
-    coloreStriscia=color(128,128,128)
+    coloreStriscia=color(100,100,190)#128
+
     def __init__(self,x,step,speed,larg,altez,testo="3C"):
         self.x = x
         self.step = step
         self.speed = speed
         self.larg = larg
         self.altezza= altez
-        #self.colore=colore
-        #self.fontSize=fontSize
-        
-        self.scritta=Scritte(testo,"red",self.altezza-1)
+        self.scritta=Scritte(testo,"green",self.altezza-(1/2))
                             
-        
     def move(self):
         self.x = self.x - self.step*self.speed
         
@@ -22,13 +19,12 @@ class mezz(object):
                     
     def display(self):
         
-        
         for i in  range(width/10):
             fill(mezz.coloreStriscia)
             rect(self.x + i*120,height/2,self.larg,self.altezza)
       
-            self.scritta.setPos(self.x + i*120 + 3,height/2+self.altezza-2)
+            self.scritta.setPos(self.x + i*120 + (7/2),height/2+self.altezza-2)
             self.scritta.display()
-        
-        
-   
+            
+    def updateSpeed(self,newSpeed):
+        self.speed=newSpeed        
